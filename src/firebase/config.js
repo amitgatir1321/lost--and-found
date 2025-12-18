7ac29239ebd020819af4d7f22ffc4739d498d56d
Amit <amitgatir1308@gmail.com>
2025-12-18 10:36:24 +0530

    update env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbs3oiqtzcOkxUzEokwvTU_Fo1gNE7XXg",
-  authDomain: "lost-and-found-7eb18.firebaseapp.com",
-  projectId: "lost-and-found-7eb18",
-  storageBucket: "lost-and-found-7eb18.firebasestorage.app",
-  messagingSenderId: "185986458650",
-  appId: "1:185986458650:web:0fec3389e2ce06b6c41da3",
-  measurementId: "G-WHDMLGB5BG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
